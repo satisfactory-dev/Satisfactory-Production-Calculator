ec2873303be4220d8a6ba4f333ac79975ed66003
SignpostMarv <me@signpostmarv.name>
2024-04-17 08:02:03 +0000

    pulling in $defs update

--- generate-Docs.json.ts
+++ generate-Docs.json.ts
@@ -49,7 +49,7 @@ try {
 	await bar.write(`${__dirname}/generated-types/update8/`);
 	performance.measure('types generated', 'bootstrap done');
 	const discovery = await bar.discovery;
-	const result = await discovery.discover_type_definitions();
+	const result = await discovery.discover_type_$defs();
 
 	process.stdout.write(
 		`${JSON.stringify(result.missing_classes, null, '\t')}\n`

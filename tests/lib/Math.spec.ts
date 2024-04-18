f9c646fa181073fd8c8e41f64e27a1b6760e7040
SignpostMarv <me@signpostmarv.name>
2024-04-18 22:39:09 +0000

    account for changes to Math

--- tests/lib/Math.spec.ts
+++ tests/lib/Math.spec.ts
@@ -14,13 +14,14 @@ import {
 import {
 	integer_string__type,
 } from '../../generated-types/update8/common/unassigned';
+import BigNumber from 'bignumber.js';
 
 void describe('Math', () => {
 	void describe('add', () => {
-		const data_sets: [number_arg, number_arg, amount_string][] = [
-			[1,2, '3' as amount_string],
-			[0, 22.5000001, '22.500001' as amount_string],
-			[0, 22.50000001, '22.5' as amount_string],
+		const data_sets: [number_arg, number_arg, string][] = [
+			[1,2, '3' as string],
+			[0, 22.5000001, '22.5000001'],
+			[0, 22.50000001, '22.50000001'],
 		];
 
 		for (const entry of data_sets) {
@@ -35,8 +36,8 @@ void describe('Math', () => {
 				}`,
 				() => {
 					assert.equal(
-						Math.add(a, b),
-						expectation
+						BigNumber(expectation).comparedTo(Math.add(a, b)),
+						0
 					);
 				}
 			)
@@ -84,14 +85,14 @@ void describe('Math', () => {
 
 	void describe('append_multiply', () => {
 		type data_set = [
-			number_arg,
+			BigNumber,
 			(
-				| number_arg
-				| [number_arg, ...number_arg[]]
-				| [amount_string, ...amount_string[]]
+				| BigNumber
+				| [BigNumber, ...BigNumber[]]
 			),
-			number_arg,
-			number_arg,
+			BigNumber,
+			BigNumber,
+			amount_string|null
 		];
 		const unmapped_expectations:[
 			string,
@@ -101,31 +102,33 @@ void describe('Math', () => {
 			),
 			string,
 			string,
+			string|null,
 		][] = [
-			['1', '2', '3', '7'],
-			['1', ['2', '3'], '4', '21'],
-			['0', '22.5', '1.000001', '22.500023'],
-			['0', '45', '1.000001', '45.000045'],
+			['1', '2', '3', '7', null],
+			['1', ['2', '3'], '4', '21', null],
+			['0', '22.5', '1.000001', '22.5000225', '22.500023'],
+			['0', '45', '1.000001', '45.000045', null],
 		];
 		const expectations:data_set[] = unmapped_expectations.map((
 			data_set
 		) : data_set => {
 			return [
-				Math.amount_string(data_set[0]),
+				BigNumber(data_set[0]),
 				(
 					data_set[1] instanceof Array
 						? require_non_empty_array(data_set[1].map(
-							(e:string) => Math.amount_string(e)
+							(e:string) => BigNumber(e)
 						))
-						: Math.amount_string(data_set[1])
+						: BigNumber(data_set[1])
 				),
-				Math.amount_string(data_set[2]),
-				Math.amount_string(data_set[3]),
+				BigNumber(data_set[2]),
+				BigNumber(data_set[3]),
+				null === data_set[4] ? null : Math.amount_string(data_set[4]),
 			];
 		});
 
 		for (const entry of expectations) {
-			const [append_to, a, b, expectation] = entry;
+			const [append_to, a, b, expectation, rounded_off] = entry;
 			void it (
 				`Math.append_multiply(${
 					append_to.toString()
@@ -144,15 +147,26 @@ void describe('Math', () => {
 					);
 
 					assert.doesNotThrow(get_result);
-					assert.equal(get_result(), expectation);
+					const result = get_result();
+					assert.equal(
+						BigNumber(expectation).comparedTo(result),
+						0,
+						`Expecting ${expectation.toString()}, receieved ${result.toString()}`
+					);
+					if (null !== rounded_off) {
+						assert.equal(
+							Math.round_off(result),
+							rounded_off
+						);
+					}
 				}
 			)
 		}
 	})
 
 	void describe('divide', () => {
-		const data_sets:[number_arg, number_arg, amount_string][] = [
-			[2, 3, '0.666667' as amount_string],
+		const data_sets:[number_arg, number_arg, string][] = [
+			[2, 3, '0.666667'],
 		];
 
 		for (const entry of data_sets) {
@@ -168,8 +182,8 @@ void describe('Math', () => {
 				}`,
 				() => {
 					assert.equal(
-						Math.divide(a, b),
-						expectation
+						BigNumber(expectation).comparedTo(Math.divide(a, b)),
+						0
 					);
 				}
 			)
@@ -207,19 +221,19 @@ void describe('Math', () => {
 	void describe('least_common_multiple', () => {
 		const data_set:[
 			[number_arg, number_arg, ...number_arg[]],
-			amount_string,
+			string,
 		][] = [
 			[
 				[1, 2],
-				'2' as integer_string__type,
+				'2',
 			],
 			[
 				[30, 50, 65], // iron ingot recipes as of April 2024
-				'1950' as integer_string__type,
+				'1950',
 			],
 			[
 				[37.5, 22.5],
-				'112.5' as amount_string,
+				'112.5',
 			],
 		];
 
@@ -231,18 +245,44 @@ void describe('Math', () => {
 				}) returns ${expectation}`,
 				() => {
 					assert.equal(
-						Math.least_common_multiple(numbers),
-						expectation
+						BigNumber(expectation).comparedTo(
+							Math.least_common_multiple(numbers)
+						),
+						0
 					);
 				}
 			)
 		}
 	})
 
+	void describe('round_off', () => {
+		const data_sets:[number_arg, string][] = [
+			[22.50000001, '22.5'],
+		];
+
+		for (const entry of data_sets) {
+			const [input, expectation] = entry;
+
+			void it(
+				`Math.round_off(${
+					input.toString()
+				}) returns ${
+					expectation
+				}`,
+				() => {
+					assert.equal(
+						Math.round_off(BigNumber(input)),
+						expectation
+					)
+				}
+			)
+		}
+	})
+
 	void describe('sub', () => {
-		const data_sets:[number_arg, number_arg, amount_string][] = [
-			[1.0000001, 0.0000001, '1' as amount_string],
-			[3, 2, '1' as amount_string],
+		const data_sets:[number_arg, number_arg, string][] = [
+			[1.0000001, 0.0000001, '1'],
+			[3, 2, '1'],
 		];
 
 		for (const entry of data_sets) {
@@ -258,8 +298,8 @@ void describe('Math', () => {
 				}`,
 				() => {
 					assert.equal(
-						Math.sub(a, b),
-						expectation
+						BigNumber(expectation).comparedTo(Math.sub(a, b)),
+						0
 					);
 				}
 			)

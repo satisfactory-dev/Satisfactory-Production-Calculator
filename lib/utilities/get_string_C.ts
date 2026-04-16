ce772b0ea7ba4d0f91c91bfd7da0f2a990c9e9bd
SignpostMarv <me@signpostmarv.name>
2026-04-16 21:40:59 +0000

    forgot to pull in some hardcoded values

--- lib/utilities/get_string_C.ts
+++ lib/utilities/get_string_C.ts
@@ -3,6 +3,12 @@ const regex_already_string_C = /^[A-Z][A-Za-z0-9_]+_C$/;
 const regex_get_string_C = /\/([^./]+).(\1_C)/i;
 
 export function get_string_C(maybe: string): `${string}_C` {
+	if ('/Script/FactoryGame.FGBuildableAutomatedWorkBench' === maybe) {
+		return 'Build_WorkBench_C';
+	} else if ('/Script/FactoryGame.FGBuildGun' === maybe) {
+		return 'BP_BuildGun_C';
+	}
+
 	if (regex_already_string_C.test(maybe)) {
 		return maybe as `${string}_C`;
 	}
